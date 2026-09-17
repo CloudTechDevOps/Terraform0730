4b516a788535fbc3279dedd86304443580202698
veera <veerababu.narni@gmail.com>
2026-09-17 08:08:37 +0530

    day-4,5 state

--- Day-5-statefile-locking/backend.tf
+++ Day-5-statefile-locking/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "statefile-store-nit"
     key    = "terraform-statefile/terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true ##supports terrafrom latest version >=1.10
+    
   }
 }
