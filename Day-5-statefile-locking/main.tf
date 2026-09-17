308dbfe4723bbcc734887de06626858b7b8b00b7
veera <veerababu.narni@gmail.com>
2026-09-17 08:21:07 +0530

    updates

--- Day-5-statefile-locking/main.tf
+++ Day-5-statefile-locking/main.tf
@@ -10,3 +10,10 @@ resource "aws_subnet" "dev"{
     cidr_block = "10.0.0.0/24"
     vpc_id = aws_vpc.name.id    
 }
+
+resource "aws_instance" "name" {
+    ami                    = "ami-0e34b50e714a297f1" #Amazon 202
+    instance_type          = "t2.medium"
+    subnet_id              = aws_subnet.dev.id
+  
+}
